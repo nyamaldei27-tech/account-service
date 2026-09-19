299edf93d56eb3c7390229e6033eca9792f1457e
Nyamal Chuol <nyamaldei27@gmail.com>
2026-09-19 21:30:42 +0300

    Added keycloack implementation with securityConfig and application properties

--- src/main/java/com/example/acount_service/controller/AccountController.java
+++ src/main/java/com/example/acount_service/controller/AccountController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
@@ -32,6 +33,29 @@ public class AccountController {
                 .body(createdCustomer);
     }
 
+
+    @GetMapping("/me")
+    public ResponseEntity<CustomerResponse> getCurrentCustomer(
+            Authentication authentication) {
+
+        return ResponseEntity.ok(
+                accountService.getCurrentCustomer(authentication)
+        );
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<CustomerResponse> updateCurrentCustomer(
+            Authentication authentication,
+            @Valid @RequestBody CustomerRequest request) {
+
+        return ResponseEntity.ok(
+                accountService.updateCurrentCustomer(
+                        authentication,
+                        request
+                )
+        );
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponse> getCustomerById(
             @PathVariable Long id) {
@@ -48,4 +72,24 @@ public class AccountController {
                 accountService.getAllCustomers()
         );
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponse> updateCustomer(
+            @PathVariable Long id,
+            @Valid @RequestBody CustomerRequest request) {
+
+        return ResponseEntity.ok(
+                accountService.updateCustomer(id, request)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(
+            @PathVariable Long id) {
+
+        accountService.deleteCustomer(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
